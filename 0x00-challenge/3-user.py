4feb20716ce1c0a396ce489dafff8bdac74c98c1
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-11-03 09:15:55 +0100

    user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
  User Model
+ 
+ Fixed issues with the code:
+    - `__password` should be private
+    - `__password` should be hashed
+    - `__password` should be None if `pwd` is None
+    - `__password` should be None if `pwd` is not a string
 """
 import hashlib
 import uuid
@@ -40,7 +46,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -52,9 +58,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
